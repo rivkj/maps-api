dfcd802cad237fd98eadac66756accf46127f6a6
kimberlyrivas <rivaskj07@gmail.com>
2022-10-04 17:02:55 -0400

    first commit

---
+++ MapsApp/src/main/java/com/tts/MapsApp/MapService.java
@@ -0,0 +1,19 @@
+package com.tts.MapsApp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MapService {
+    @Value("${api_key}")
+    private String apiKey;
+
+    public void addCoordinates(com.tts.MapsApp.Location location) {
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+        location.getCity() + "," + location.getState() + "&key=" + apiKey;
+        
+        RestTemplate restTemplate = new RestTemplate();
+        GeocodingReponse response = restTemplate.getForObject(url, GeocodingReponse.class);
+    }
+}
